68e159affbd539c30decddd2ad651f48ca807443
guilrom <guilrom@gmail.com>
2017-05-08 15:26:28 +0200

    correcting indentations, codding styles...

--- package.js
+++ package.js
@@ -5,20 +5,20 @@ Package.describe({
   git: 'https://github.com/guilrom/meteor-autoform-tags-toggle'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.0');
 
   api.use([
-  	'templating',
-  	'reactive-var',
-  	'coffeescript',
-  	'aldeed:autoform@5.5.1'
+    'templating',
+    'reactive-var',
+    'coffeescript',
+    'aldeed:autoform@5.5.1'
   ], 'client');
 
   api.addFiles([
-  	'lib/client/autoform-tags-toggle.html',
-    'lib/client/autoform-tags-toggle.coffee',  	
-  	'lib/client/autoform-tags-toggle.css'
-  ], 'client');  
-  
+    'lib/client/autoform-tags-toggle.html',
+    'lib/client/autoform-tags-toggle.coffee',
+    'lib/client/autoform-tags-toggle.css'
+  ], 'client');
+
 });
