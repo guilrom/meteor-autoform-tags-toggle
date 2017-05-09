bbd9dd083a32edd014a7ab46ea4746a0c999aa09
guilrom <guilrom@gmail.com>
2017-05-09 13:54:05 +0200

    Reverting useless change

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'guilrom:autoform-tags-toggle2',
+  name: 'guilrom:autoform-tags-toggle',
   summary: 'Select tags with autoForm',
   version: '0.0.1',
   git: 'https://github.com/guilrom/meteor-autoform-tags-toggle'
